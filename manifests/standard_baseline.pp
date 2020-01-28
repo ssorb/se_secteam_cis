f75efe009cf37700778b13ca78497a5d26f3cb8d
murdok5 <mike.smith@puppet.com>
2020-01-28 15:18:15 -0800

    adding rhel controls

--- manifests/standard_baseline.pp
+++ manifests/standard_baseline.pp
@@ -1,6 +1,7 @@
 # @summary Entry point for Security Team's CIS module.
 #
-# This module enforces CIS benchmark settings for Windows 2019 and Centos 8 servers. There is a single parameter to optionally set noop mode for the scope of this class.
+# This module enforces CIS benchmark settings for Windows 2019 and Centos 8 servers. 
+# There is a single parameter to optionally set noop mode for the scope of this class.
 #
 # @example
 #   include secteam::standard_baseline
@@ -9,11 +10,11 @@ class secteam::standard_baseline {
   $fam = $facts.get('os.family').tolower
   $maj = $facts.get('os.release.major').tolower
 
-  if defined("secteam::${fam}::${maj}") {
-    include "secteam::${fam}::${maj}"
+  if defined("secteam_cis::${fam}::${maj}") {
+    include "secteam_cis::${fam}::${maj}"
   }
   else {
-    fail('secteam - unsupported operating system, please see readme')
+    fail('secteam_cis - unsupported operating system, please see readme')
   }
 
 }
