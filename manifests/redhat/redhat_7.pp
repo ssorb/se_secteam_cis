d65e40bd096e4402d99314ca8acab8bb27e7101d
murdok5 <mike.smith@puppet.com>
2020-08-17 13:09:36 -0700

    restructure classes

--- manifests/redhat/redhat_7.pp
+++ manifests/redhat/redhat_7.pp
@@ -7,7 +7,7 @@
 # @example
 #   include secteam::os::rhel_8
 
-class secteam_cis::RedHat::7 (
+class secteam_cis::redhat::redhat_7 (
 
   Boolean $firewall_enabled = true,
   String  $password_history = '50',
--- manifests/standard_baseline.pp
+++ manifests/standard_baseline.pp
@@ -10,8 +10,8 @@ class secteam_cis::standard_baseline {
   $fam = $facts.get('os.family').downcase()
   $maj = $facts.get('os.release.major').downcase()
 
-  if defined("secteam_cis::${fam}::${maj}") {
-    include "secteam_cis::${fam}::${maj}"
+  if defined("secteam_cis::${fam}::${fam}_${maj}") {
+    include "secteam_cis::${fam}::${fam}_${maj}"
   }
   else {
     fail('secteam_cis - unsupported operating system, please see readme')
